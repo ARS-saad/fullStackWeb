2b0331b79f6428ffd2b9b92940a6260d5e5524f6
ARS-saad <abdurrahmansaad46@gmail.com>
2024-08-22 15:21:32 +0600

    finish my work

--- server/app.js
+++ server/app.js
@@ -3,9 +3,11 @@ const create = require("./lib/create");
 const read = require("./lib/read");
 const update = require("./lib/update");
 const deleted = require("./lib/delete");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.route("/").post(create).get(read).put(update).delete(deleted);
